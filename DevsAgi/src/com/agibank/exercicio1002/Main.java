c04c528b63797dd7507934f30b21bff4e7b24270
Vitor Coelho <130684606+vijcoelho@users.noreply.github.com>
2025-02-10 18:18:19 -0300

    commit exercicio 8 ao 10

--- DevsAgi/src/com/agibank/exercicio1002/Main.java
+++ DevsAgi/src/com/agibank/exercicio1002/Main.java
@@ -1,5 +1,7 @@
 package com.agibank.exercicio1002;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 //        double real = 100;
@@ -10,6 +12,7 @@ public class Main {
 //        System.out.printf("Crescimento: %.1f%%", calcularCrescimento(100, 105));
 
 //        double[] vetor = {100, 102, 101, 103, 105};
+//        System.out.print("Media movel: ");
 //        calcularMediaMovel(vetor, 3);
 
 //        System.out.println(comprarAtivos(5,4));
@@ -20,6 +23,22 @@ public class Main {
 //        double[] valores = {1000, 2000, 1500};
 //        double[] ativos = {0.05, 0.03, 0.04};
 //        System.out.println(simularCarteira(valores, ativos));
+
+//        System.out.printf("Montante final: %.2f", calcularJurosCompostos(1000, 2, 12));
+
+//        double[] serie1 = {100, 102, 104, 106};
+//        double[] serie2 = {50, 51, 52, 53};
+//        System.out.printf("Correlacao: %.2f", calcularCorrelacao(serie1, serie2));
+
+//        double[] precosSimulados = simularPrecoMonteCarlo(
+//                100,
+//                0.001,
+//                0.02,
+//                10
+//        );
+//        for (double i : precosSimulados) {
+//            System.out.printf("%.2f ", i);
+//        }
     }
 
     // Exercicio 1
@@ -37,7 +56,7 @@ public class Main {
         if (preco1 >= preco2) {
             return 0;
         }
-        return ((preco2 - preco1) / 100) * 100;
+        return ((preco2 - preco1) / preco1) * 100;
     }
 
     // Exercicio 4
@@ -50,16 +69,16 @@ public class Main {
             mediaMovel[i] += array[j] / n;
             j++;
             count++;
-            if (count % n == 0) {
+            if (j == array.length) {
+                break;
+            } else if (count % n == 0) {
                 i++;
                 j = i;
-            } else if (j == array.length) {
-                break;
             }
         }
 
-        for (int k = 0; k < n; k++) {
-            System.out.println(mediaMovel[k]);
+        for (int k = 0; k < mediaMovel.length; k++) {
+            System.out.print(mediaMovel[k] + " ");
         }
     }
 
@@ -102,5 +121,65 @@ public class Main {
         return total;
     }
 
-    //Exercicio 8
+    // Exercicio 8
+    public static double calcularJurosCompostos(double capital, double taxa, int meses) {
+        return capital * Math.pow((1 + (taxa / 100)), meses);
+    }
+
+    // Exercicio 9
+    public static double calcularCorrelacao(double[] serie1, double[] serie2) {
+        if (serie1.length != serie2.length) {
+            return 0.0;
+        }
+
+        double media1 = 0, media2 = 0;
+
+        for (double i : serie1) {
+            media1 += i / serie1.length;
+        }
+        for (double i : serie2) {
+            media2 += i / serie2.length;
+        }
+
+        double desvioPadraoSerie1 = 0;
+        double potencia = 0;
+        for (double v : serie1) {
+            potencia += Math.pow((v - media1), 2);
+        }
+        desvioPadraoSerie1 = Math.sqrt((double) 1 / (serie1.length - 1) * potencia);
+
+        double desvioPadraoSerie2 = 0;
+        double potencia2 = 0;
+        for (double v : serie2) {
+            potencia2 += Math.pow((v - media2), 2);
+        }
+        desvioPadraoSerie2 = Math.sqrt((double) 1 / (serie2.length - 1) * potencia2);
+
+        double covariancia = 0;
+        for (int i = 0; i < serie1.length; i++) {
+            covariancia += (serie1[i] - media1) * (serie2[i] - media2);
+        }
+        covariancia /= (serie1.length - 1);
+
+        return covariancia / (desvioPadraoSerie1 * desvioPadraoSerie2);
+    }
+
+    // Exercicio 10
+    public static double[] simularPrecoMonteCarlo(
+            double precoInicial,
+            double mediaRetornoDiario,
+            double volatilidade,
+            int periodo
+    ) {
+        Random random = new Random();
+        double[] precos = new double[periodo + 1];
+        precos[0] = precoInicial;
+
+        for (int i = 1; i < precos.length ; i++) {
+            double z = random.nextGaussian();
+            precos[i] = precos[i - 1] * Math.exp(mediaRetornoDiario + volatilidade * z);
+        }
+
+        return precos;
+    }
 }
